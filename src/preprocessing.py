e7be0248520d77c3ecfc754af8e6d1f312ce38de
Baptiste <picard.baptiste22@gmail.com>
2025-02-13 10:44:38 +0100

    Adding utilitary functions

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -89,6 +89,35 @@ def scale_and_encoder_features(df: pd.DataFrame) -> Tuple[pd.DataFrame, Dict[Tex
     return scaled_and_encoded_df, encoders_and_scalers
 
 
+def encode_categories_using_encoders_and_scalers(df: pd.DataFrame, encoders_and_scalers: Dict[Text, Union[LabelEncoder, OneHotEncoder, StandardScaler, MinMaxScaler]]) -> pd.DataFrame:
+    """Use encoder to properly encode the DataFrame columns using trained encoders and scalers.
+
+    :param df:
+    :param encoders_and_scalers:
+    :return pd.DataFrame:
+    """
+    # Creating a copy.
+    to_encode_df = df.copy()
+    # Iterating over columns.
+    for column_name in to_encode_df.columns:
+        if column_name in ["id", "index"]:
+            continue
+        elif to_encode_df[column_name].dtype in ["object", "category"]:
+            to_encode_df[column_name] = (
+                encoders_and_scalers[column_name]
+                .transform(to_encode_df.loc[:, [column_name]])
+            )
+        elif to_encode_df[column_name].dtype == "float64":
+            to_encode_df[column_name] = (
+                encoders_and_scalers[column_name]
+                .transform(to_encode_df.loc[:, [column_name]].to_numpy())
+            )
+        else:
+            raise TypeError(f"{to_encode_df[column_name].dtype}")
+    return to_encode_df
+
+
+
 def create_polynomial_features(df: pd.DataFrame, polynomial_degree=3) -> Tuple[pd.DataFrame, PolynomialFeatures]:
     """Use the PolynomialFeatures transformer to process input.
 
@@ -106,6 +135,24 @@ def create_polynomial_features(df: pd.DataFrame, polynomial_degree=3) -> Tuple[p
         columns=["Original"] + [f"polynomial_{index}" for index in range(1, polynomial_feature_values.shape[1])],
     )
     return new_df.reset_index().rename(columns={"index": "id"}), poly_enc
+
+
+def generate_polynomial_column_using_polynomial_feature_encoder(df: pd.DataFrame, input_column_name: Text, polynomial_encoder: PolynomialFeatures) -> pd.DataFrame:
+    """Use trained PolynomialFeatures to generate polynomial columns.
+
+    :param df:
+    :param polynomial_encoder:
+    :return pd.DataFrame:
+    """
+    # Creating a copy.
+    df_polynomial = df.copy().set_index(keys="id")
+    indexes = np.asarray(df_polynomial.index).astype(int)
+    polynomial_feature_values = polynomial_encoder.transform(df_polynomial.loc[:, [input_column_name]])
+    return pd.DataFrame(
+        data=polynomial_feature_values,
+        index=indexes,
+        columns=["Original"] + [f"polynomial_{index}" for index in range(1, polynomial_feature_values.shape[1])],
+    ).reset_index().rename(columns={"index": "id"})
 
 
 def split_X_y_in_train_test_sets(X: pd.DataFrame, y: pd.Series) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
